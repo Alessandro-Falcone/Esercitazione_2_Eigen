a24c0b9140c381ca6a1baa00855477b2b6e769c5
alexf <alexf@LAPTOP-L8J25IH7>
2024-03-30 17:12:29 +0100

    main modificato

--- Exercise2/main.cpp
+++ Exercise2/main.cpp
@@ -30,10 +30,10 @@ void testSoluzione(const MatrixXd& A, const VectorXd& b, const VectorXd& soluzio
 
 
     errRelPALU = numeric_limits<double>::max();
-    // massimo valore che può assumere un double assegnato all'errore relativo per la fattorizzazione PALU
+    // massimo valore che può assumere un double, assegnato all'errore relativo per la fattorizzazione PALU
 
     errRelQR = numeric_limits<double>::max();
-    // massimo valore che può assumere un double assegnato all'errore relativo per la fattorizzazione QR
+    // massimo valore che può assumere un double, assegnato all'errore relativo per la fattorizzazione QR
 
     // soluzioneSistemaPALU(A,b)
 
@@ -48,17 +48,17 @@ int main(){
 
     Vector2d soluzione(-1.0000e+0, -1.0000e+00);
     Matrix2d A1 = Matrix2d::Zero();
-    A1 << 5.547001962252291e-01, -3.770900990025203e-02,8.320502943378437e-01,-9.992887623566787e-01;
+    A1 << 5.547001962252291e-01, -3.770900990025203e-02, 8.320502943378437e-01, -9.992887623566787e-01;
     Vector2d b1 = Vector2d::Zero();
     b1 << -5.169911863249772e-01, 1.672384680188350e-01;
 
     Matrix2d A2 = Matrix2d::Zero();
-    A2 << 5.547001962252291e-01, -5.540607316466765e-01,8.320502943378437e-01,-8.324762492991313e-01;
+    A2 << 5.547001962252291e-01, -5.540607316466765e-01, 8.320502943378437e-01, -8.324762492991313e-01;
     Vector2d b2 = Vector2d::Zero();
     b2 << -6.394645785530173e-04, 4.259549612877223e-04;
 
     Matrix2d A3 = Matrix2d::Zero();
-    A3 << 5.547001962252291e-01, -5.547001955851905e-01,8.320502943378437e-01,-8.320502947645361e-01;
+    A3 << 5.547001962252291e-01, -5.547001955851905e-01, 8.320502943378437e-01, -8.320502947645361e-01;
     Vector2d b3 = Vector2d::Zero();
     b3 << -6.400391328043042e-10, 4.266924591433963e-10;
 
